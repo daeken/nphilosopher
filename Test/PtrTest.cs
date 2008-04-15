a8b6b459609a70b50cccf05c5b30a0a5249fa69b
cody.brocious <cody.brocious@63db5cba-574a-0410-a563-5937a96bb1a4>
2008-04-15 22:49:55 +0000

    Fixed tons of bugs. Added static field support.

--- Test/PtrTest.cs
+++ Test/PtrTest.cs
@@ -1,22 +1,23 @@
 class PtrTest {
+	unsafe static byte *PutChar(char ch, byte *ptr) {
+		ptr[0] = (byte) ch;
+		ptr[1] = 0x0F;
+		return ptr + 2;
+	}
+	
 	unsafe static public void Main() {
 		byte *ptr = (byte *) 0xB8000U;
-		*ptr = (byte) 'H'; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) 'e'; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) 'l'; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) 'l'; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) 'o'; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) ' '; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) 'W'; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) 'o'; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) 'r'; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) 'l'; ptr++; *ptr = 0x0F; ptr++;
-		*ptr = (byte) 'd'; ptr++; *ptr = 0x0F; ptr++;
 		
-		int j = 0;
-		for(int i = 0; i < 10; ++i) {
-			j++;
-		}
+		ptr = PutChar('C', ptr);
+		ptr = PutChar('o', ptr);
+		ptr = PutChar('l', ptr);
+		ptr = PutChar('o', ptr);
+		ptr = PutChar('r', ptr);
+		ptr = PutChar('B', ptr);
+		ptr = PutChar('l', ptr);
+		ptr = PutChar('i', ptr);
+		ptr = PutChar('n', ptr);
+		ptr = PutChar('d', ptr);
 		
 		while(true) {}
 	}
